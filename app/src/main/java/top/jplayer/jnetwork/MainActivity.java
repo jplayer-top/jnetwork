27eb27e393f62c56fe737581f6c42d977e5cb05a
Administrator <admin@example.com>
2020-03-24 16:02:44 +0800

    子弹飞一飞，代码跑一跑

--- app/src/main/java/top/jplayer/jnetwork/MainActivity.java
+++ app/src/main/java/top/jplayer/jnetwork/MainActivity.java
@@ -1,14 +1,34 @@
 package top.jplayer.jnetwork;
 
 import androidx.appcompat.app.AppCompatActivity;
+import top.jplayer.codelib.AutoWired;
+import top.jplayer.jnetwork.presenter.MainPresenter;
+import top.jplayer.networklibrary.contract.IContract;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements IContract.IView {
+    @AutoWired
+    public MainPresenter mMainPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void showError() {
+
+    }
+
+    @Override
+    public void showLoading() {
+
+    }
+
+    @Override
+    public void showEmpty() {
+
     }
 }
--- app/src/main/java/top/jplayer/jnetwork/presenter/MainPresenter.java
+++ app/src/main/java/top/jplayer/jnetwork/presenter/MainPresenter.java
@@ -1,10 +1,16 @@
 package top.jplayer.jnetwork.presenter;
 
+import top.jplayer.jnetwork.MainActivity;
+import top.jplayer.networklibrary.contract.BasePresenter;
+
 /**
  * Created by Obl on 2020/3/24.
  * top.jplayer.jnetwork.presenter
  * call me : jplayer_top@163.com
  * github : https://github.com/oblivion0001
  */
-public class MainPresenter {
+public class MainPresenter extends BasePresenter<MainActivity> {
+    public MainPresenter(MainActivity iView) {
+        super(iView);
+    }
 }
